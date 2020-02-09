eade86504c576f949b558422f9bab0a83b7bd455
jeffyactive <jeff@reelyactive.com>
2020-02-09 14:57:41 -0500

    Added eddystone.js

---
+++ js/eddystone.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright reelyActive 2020
+ * We believe in an open Internet of Things
+ */
+
+
+let eddystone = (function() {
+
+  // Internal constants
+
+  // Internal variables
+
+  // Expose the following functions and variables
+  return {
+
+  }
+
+}());
