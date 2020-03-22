cb9f6b042ebc93bb76eebb4fdb96168cede8acb7
jeffyactive <jeff@reelyactive.com>
2020-03-21 21:47:00 -0400

    Parse Minew data

--- js/minew.js
+++ js/minew.js
@@ -38,39 +38,71 @@ let minew = (function() {
   // Parse the given temperature and humidity data
   function parseTemperatureHumidity(transmitterSignature, serviceData,
                                     deviceData) {
-    let temperature = 0;
-    let humidityPercentage = 0;
-    let batteryPercentage = 100;
+    let batteryPercentage = serviceData[2];
+    let temperature = toDecimal(serviceData[3], serviceData[4]);
+    let humidityPercentage = toDecimal(serviceData[5], serviceData[6]);
+    let macAddress = toMacAddress(serviceData, 7);
     let data = { temperature: temperature,
                  humidityPercentage: humidityPercentage,
-                 batteryPercentage: batteryPercentage };
+                 batteryPercentage: batteryPercentage,
+                 macAddress: macAddress };
     deviceData.push(data);
   }
 
   // Parse the given visible light data
   function parseVisibleLight(transmitterSignature, serviceData, deviceData) {
-    let visibleLight = true;
-    let batteryPercentage = 100;
+    let batteryPercentage = serviceData[2];
+    let visibleLight = (serviceData[3] > 0);
+    let macAddress = toMacAddress(serviceData, 4);
     let data = { visibleLight: visibleLight,
-                 batteryPercentage: batteryPercentage };
+                 batteryPercentage: batteryPercentage,
+                 macAddress: macAddress };
     deviceData.push(data);
   }
 
   // Parse the given accelerometer data
   function parseAccelerometer(transmitterSignature, serviceData, deviceData) {
-    let acceleration = [ 0, 0, 0 ];
-    let batteryPercentage = 100;
+    let batteryPercentage = serviceData[2];
+    let acceleration = [ toDecimal(serviceData[3], serviceData[4]),
+                         toDecimal(serviceData[5], serviceData[6]),
+                         toDecimal(serviceData[7], serviceData[8]) ];
+    let macAddress = toMacAddress(serviceData, 9);
     let data = { acceleration: acceleration,
-                 batteryPercentage: batteryPercentage };
+                 batteryPercentage: batteryPercentage,
+                 macAddress: macAddress };
     deviceData.push(data);
   }
 
   // Parse the given info data
   function parseInfo(transmitterSignature, serviceData, deviceData) {
-    let name = 'PLUS';
-    let batteryPercentage = 100;
-    let data = { name: name, batteryPercentage: batteryPercentage };
+    let batteryPercentage = serviceData[2];
+    let macAddress = toMacAddress(serviceData, 3);
+    let name = 'PLUS';  // TODO: make dynamic
+    let data = { name: name, batteryPercentage: batteryPercentage,
+                 macAddress: macAddress };
     deviceData.push(data);
+  }
+
+  // Convert the given signed 8.8 fixed-point bytes to decimal.
+  function toDecimal(integerByte, decimalByte) {
+    let integer = integerByte;
+    let decimal = decimalByte / 256;
+
+    if(integer > 127) {
+      return (integer - 256) + decimal;
+    }
+    return integer + decimal;
+  }
+
+  // Convert the given bytes to a MAC address
+  function toMacAddress(bytes, startIndex) {
+    let macAddress = '';
+
+    for(let cByte = 5; cByte >=; cByte--) {
+      macAddress += ('0' + bytes[startIndex + cByte].toString(16)).substr(-2);
+    }
+
+    return macAddress;
   }
 
   // Expose the following functions and variables
