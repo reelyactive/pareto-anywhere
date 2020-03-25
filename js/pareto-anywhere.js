a5e145e9af294389aafde614e5126753b12cc127
jeffyactive <jeff@reelyactive.com>
2020-03-24 22:00:56 -0400

    Test with cloneNode

--- js/pareto-anywhere.js
+++ js/pareto-anywhere.js
@@ -277,9 +277,17 @@ function updateProximityCards() {
 
   sortedArray.forEach(function(device, index) {
     if(index < numberOfDevicesToDisplay) {
-      let card = document.createElement('div');
-      card.setAttribute('id', device.id);
-      card.setAttribute('class', 'card my-4');
+      let existingCard = document.querySelector('#' + device.id);
+      let card;
+
+      if(existingCard) {
+        card = existingCard.cloneNode(true);
+      }
+      else {
+        card = document.createElement('div');
+        card.setAttribute('id', device.id);
+        card.setAttribute('class', 'card my-4');
+      }
       updatedFragment.appendChild(card);
       numberOfDisplayedCards++;
       cuttlefish.renderAsTabs(card, device.stories, device.data,
